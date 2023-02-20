d74b7a6f0f7b04f45a4c80dacca83f66a38f4194
Piotr Bejger <piotr.bejger@gmail.com>
2023-02-20 22:20:59 +0100

    Trait attribute for event handling integration tests

--- tests/EventForging.CosmosDb.Tests/EventHandling_tests.cs
+++ tests/EventForging.CosmosDb.Tests/EventHandling_tests.cs
@@ -10,6 +10,7 @@ using User = EventForging.DatabaseIntegrationTests.Common.User;
 
 namespace EventForging.CosmosDb.Tests;
 
+[Trait("Category", "Integration")]
 public sealed class EventHandling_tests : IAsyncLifetime
 {
     private const string ConnectionString = "AccountEndpoint=https://localhost:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
